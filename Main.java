a9ebeca42296901071d15ec31c5021640123683e
oritsviely <oritz2008@gmail.com>
2026-01-19 15:05:05 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,66 +6,45 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-// הגדרת מבנה של צומת ברשימה מקושרת
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int val) {
-        this.val = val;
-    }
-}
-
-public class Main {
-
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // יצירת צומת דמה כדי להתחיל ממנו
-        ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
-
-        // כל עוד שתי הרשימות אינן ריקות
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                current.next = l1;
-                l1 = l1.next;
-            } else {
-                current.next = l2;
-                l2 = l2.next;
+public class Main{
+    public static Integer ex1(Queue<Integer>q){
+        int max=0;
+        int a=0;
+        while(!q.isEmpty()){
+            a=q.head();
+            q.remove();
+            if(a==0){
+                //System.out.print(q);
+                return (max);
             }
-            current = current.next;
+            if(a>q.head()){
+                if(a>max){
+                    max=a;
+                }
+                q.remove();
+            }
+            if(q.head()>max){
+                max=q.head();
+            }
+                q.remove();
         }
-
-        // אם נשארו איברים באחת הרשימות
-        if (l1 != null) {
-            current.next = l1;
-        } else {
-            current.next = l2;
-        }
-
-        // ראש הרשימה החדשה הוא אחרי הצומת הדמה
-        return dummy.next;
+    return (max);
     }
-
-    // מתודת main להדגמה
     public static void main(String[] args) {
-        // רשימה ראשונה: 1 -> 3 -> 5
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(3);
-        l1.next.next = new ListNode(5);
-
-        // רשימה שנייה: 1 -> 3 -> 4
-        ListNode l2 = new ListNode(1);
-        l2.next = new ListNode(3);
-        l2.next.next = new ListNode(4);
-
-        // מיזוג הרשימות
-        ListNode merged = mergeTwoLists(l1, l2);
-
-        // הדפסת הרשימה הממוזגת
-        System.out.print("Merged list: ");
-        while (merged != null) {
-            System.out.print(merged.val + " ");
-            merged = merged.next;
-        }
-        // פלט צפוי: Merged list: 1 1 3 3 4 5
+    Queue<Integer> q=new Queue<>();
+    int a=5;
+    q.insert(10);
+    q.insert(12);
+    q.insert(7);
+    q.insert(10);
+    q.insert(0);
+    q.insert(-5);
+    q.insert(8);
+    q.insert(2);
+    q.insert(0);
+    System.out.print(ex1(q));
+    System.out.print(q);
+    
     }
 }
+ 
